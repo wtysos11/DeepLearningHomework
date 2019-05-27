5e4bddfadefc406adcc26d18b979166dc480f73f
wtysos11 <wtysos11@163.com>
2019-05-27 15:18:58 +0800

    并行化处理完成

--- homework2/compare.py
+++ homework2/compare.py
@@ -1,7 +1,7 @@
 import os
 
 os.chdir('E:\\code\\homework\\deepLearning\\homework2')
-
+from multiprocessing import freeze_support
 #pandas用于处理数据
 import pandas as pd                                                         
 import numpy as np
@@ -14,78 +14,91 @@ from sklearn.tree import DecisionTreeRegressor
 import tree
 import time
 from sklearn.metrics import r2_score
-dataSet = pd.read_csv('train1.csv',header=None)
-labelSet = pd.read_csv('label1.csv',header=None)
-
-X_train, X_test, y_train, y_test = train_test_split(dataSet, labelSet, test_size = 0.05, random_state = 0)
-'''
-X_train = X_train[:10000]
-y_train = y_train[:10000]
-X_test = X_test[:1000]
-y_test = y_test[:1000]
-'''
-
-myTree = tree.RandomForestRegressor(n_estimators=10,max_depth = 5)
-begin = time.time()
-myTree.fit(X_train,y_train)
-end = time.time()
-print('my time:',end-begin)
-y_pred = myTree.predict(X_test)
-r2_score(y_test,y_pred)
-
-skLearnTree = RandomForestRegressor(n_estimators = 10,max_features = 3,max_depth = 5,random_state = 0)
-begin = time.time()
-skLearnTree.fit(X_train,y_train)
-end = time.time()
-print('sklearn time:',end-begin)
-y_pred = skLearnTree.predict(X_test)
-r2_score(y_test,y_pred)
-'''
-展开
-data = X_train
-label = y_train
-_min_leaf_sz = 10
-myTree = tree.DecisionTreeRegressor(2)
-treeNode = tree.DecisionTreeRegressor.DecisionTreeNode(myTree._get_data(data),False,None,myTree._get_features(data.columns),0)
 
 
-splitDiff = 0
-isSplit = True
-for col in treeNode.featureCol:
-    aimData = data.values[treeNode.dataRow,col]
-    sorted_idx = np.argsort(aimData)
-    sorted_data = aimData[sorted_idx]
-    sorted_label = label.values[sorted_idx,0]
-    lchild_number = 0
-    lchild_sum = 0
-    lchild_square_sum = 0
-    rchild_number = len(sorted_label)
-    rchild_sum = sorted_label.sum()
-    rchild_square_sum = (sorted_label**2).sum()
-    node_number = rchild_number
-    node_sum = rchild_sum
-    node_square_sum = rchild_square_sum
-    for i in range(0,node_number-_min_leaf_sz):
-        xi , yi = sorted_data[i],sorted_label[i]
-        rchild_number -= 1
-        rchild_sum -= yi
-        rchild_square_sum -= yi**2
-        lchild_number += 1
-        lchild_sum += yi
-        lchild_square_sum += yi**2
-        if i < _min_leaf_sz or xi == sorted_data[i+1]:
-            continue
-        lchild_mse = myTree._compute_mse(lchild_square_sum,lchild_sum,lchild_number)
-        rchild_mse = myTree._compute_mse(rchild_square_sum,rchild_sum,rchild_number)
-        split_mse = (lchild_number*lchild_mse + rchild_number * rchild_mse)/node_number
-        if split_mse < treeNode.splitJudge:
-            treeNode.splitCol = col
-            treeNode.splitVal = xi
-            treeNode.splitJudge = split_mse
-            splitDiff = node_number * (myTree._compute_mse(node_square_sum,node_sum,node_number) - split_mse)
-            
-lChildRow = [x for x in treeNode.dataRow if data[treeNode.splitCol].iloc[x] < treeNode.splitVal]
-rChildRow = [x for x in treeNode.dataRow if data[treeNode.splitCol].iloc[x] >= treeNode.splitVal]
-anotherTreeNode = tree.DecisionTreeRegressor.DecisionTreeNode(lChildRow,False,treeNode,treeNode.featureCol,treeNode.depth+1)
+if __name__ == "__main__":
+    freeze_support()
+    dataSet = pd.read_csv('train1.csv',header=None)
+    labelSet = pd.read_csv('label1.csv',header=None)
+    
+    X_train, X_test, y_train, y_test = train_test_split(dataSet, labelSet, test_size = 0.05, random_state = 0)
 
-'''
+    X_train = X_train[:10000]
+    y_train = y_train[:10000]
+    X_test = X_test[:1000]
+    y_test = y_test[:1000]
+
+    
+    ssX = StandardScaler()
+    X_train = ssX.fit_transform(X_train)
+    ssY = StandardScaler()
+    y_train = ssY.fit_transform(y_train)
+    X_test = ssX.transform(X_test)
+    y_test = ssY.transform(y_test)
+    
+    
+    myTree = tree.RandomForestRegressor(n_estimators=10,max_depth = 5,n_jobs = 4)
+    begin = time.time()
+    myTree.fit(X_train,y_train)
+    end = time.time()
+    print('my time:',end-begin)
+
+    y_pred = myTree.predict(X_test)
+    print(r2_score(y_test,y_pred))
+    
+    skLearnTree = RandomForestRegressor(n_estimators = 10,max_features = 3,max_depth = 5,random_state = 0,n_jobs=-1)
+    begin = time.time()
+    skLearnTree.fit(X_train,y_train.ravel())
+    end = time.time()
+    print('sklearn time:',end-begin)
+    y_pred = skLearnTree.predict(X_test)
+    print(r2_score(y_test,y_pred))
+    '''
+    展开
+    data = X_train
+    label = y_train
+    _min_leaf_sz = 10
+    myTree = tree.DecisionTreeRegressor(2)
+    treeNode = tree.DecisionTreeRegressor.DecisionTreeNode(myTree._get_data(data),False,None,myTree._get_features(data.columns),0)
+    
+    
+    splitDiff = 0
+    isSplit = True
+    for col in treeNode.featureCol:
+        aimData = data.values[treeNode.dataRow,col]
+        sorted_idx = np.argsort(aimData)
+        sorted_data = aimData[sorted_idx]
+        sorted_label = label.values[sorted_idx,0]
+        lchild_number = 0
+        lchild_sum = 0
+        lchild_square_sum = 0
+        rchild_number = len(sorted_label)
+        rchild_sum = sorted_label.sum()
+        rchild_square_sum = (sorted_label**2).sum()
+        node_number = rchild_number
+        node_sum = rchild_sum
+        node_square_sum = rchild_square_sum
+        for i in range(0,node_number-_min_leaf_sz):
+            xi , yi = sorted_data[i],sorted_label[i]
+            rchild_number -= 1
+            rchild_sum -= yi
+            rchild_square_sum -= yi**2
+            lchild_number += 1
+            lchild_sum += yi
+            lchild_square_sum += yi**2
+            if i < _min_leaf_sz or xi == sorted_data[i+1]:
+                continue
+            lchild_mse = myTree._compute_mse(lchild_square_sum,lchild_sum,lchild_number)
+            rchild_mse = myTree._compute_mse(rchild_square_sum,rchild_sum,rchild_number)
+            split_mse = (lchild_number*lchild_mse + rchild_number * rchild_mse)/node_number
+            if split_mse < treeNode.splitJudge:
+                treeNode.splitCol = col
+                treeNode.splitVal = xi
+                treeNode.splitJudge = split_mse
+                splitDiff = node_number * (myTree._compute_mse(node_square_sum,node_sum,node_number) - split_mse)
+                
+    lChildRow = [x for x in treeNode.dataRow if data[treeNode.splitCol].iloc[x] < treeNode.splitVal]
+    rChildRow = [x for x in treeNode.dataRow if data[treeNode.splitCol].iloc[x] >= treeNode.splitVal]
+    anotherTreeNode = tree.DecisionTreeRegressor.DecisionTreeNode(lChildRow,False,treeNode,treeNode.featureCol,treeNode.depth+1)
+    
+    '''
--- homework2/tree.py
+++ homework2/tree.py
@@ -3,31 +3,42 @@ from multiprocessing import Pool
 import numpy as np
 import pandas as pd
 import math
+from sklearn.tree import DecisionTreeRegressor
 
 class RandomForestRegressor:
     '''
     Random Forest Regressor
     '''
 
-    def __init__(self,n_estimators = 100,max_depth = 2,n_jobs = -1):
+    def __init__(self,n_estimators = 100,max_depth = 5,n_jobs = -1):
         self._n_estimators = n_estimators
         self._n_jobs = n_jobs
         self._max_depth = max_depth
         self._trees = [] #生成树列表
 
-
+    def _single_tree_fit(self,tree):
+        return tree.fit(self.data,self.label)
 
     # 训练随机森林集合（并行化处理）
     def fit(self,data,label):
         '''
-        data和label均为pandas.DataFrame结构
+        data和label均为numpy.array结构
         考虑到数据存放问题，在本函数内处理数据，并并行化地调用训练生成树
         '''
+        self.data = data
+        self.label = label
         for i in range(self._n_estimators):
-            myTree = DecisionTreeRegressor(self._max_depth)
-            myTree.fit(data,label)
-            self._trees.append(myTree)
-            print(i)
+            self._trees.append(MyDecisionTreeRegressor(self._max_depth))
+
+        if self._n_jobs == 0:
+            for tree in self._trees:
+                self._single_tree_fit(tree)
+        else:
+            worker = self._n_jobs
+            if worker == -1:
+                worker = 8
+            pool = Pool(processes = worker)
+            self._trees = list(pool.map(self._single_tree_fit,self._trees))
 
 
     # 根据得到的生成树集合进行预测并返回结果
@@ -45,7 +56,7 @@ class RandomForestRegressor:
 lg2 = math.log(2)
 
 #决策树回归器
-class DecisionTreeRegressor:
+class MyDecisionTreeRegressor:
     '''
         决策树训练过程：
             自助法随机产生一个数据集合，用来作为本决策树的数据
@@ -68,40 +79,9 @@ class DecisionTreeRegressor:
                     
     '''
 
-    class DecisionTreeNode:
-        '''
-        决策树结构设计：
-            决策树的根节点保存在root中
-            决策树的每个节点有三个指针，指向parent，以及左右孩子
-            决策树的每个节点保留自己分类的特征与范围（回归树节点）
-            保留了数据的个数
-            保留了深度
-
-            需要保留数据的索引列表（行）以访问数据，不保存数据原本
-
-            如果数据是叶节点，还需要保存平均值作为最后查询时的结果
-
-            如果没有特殊情况，切分时左子树永远小于右子树（所以等于在右边，如果有的话）
-        '''
-        def __init__(self,dataRow,isLeaf,parent,featureCol,depth):
-            self.dataRow = dataRow #拿到的自助法抽到的行的索引
-            self.featureCol = featureCol #拿到的基于随机森林思想的特征列的索引
-            self.featureNum = len(featureCol)
-
-            self.isLeaf = isLeaf #是否为叶节点
-            self.parent = parent
-            self.lChild = None
-            self.rChild = None
-            self.splitCol = 0 #进行分隔的特征的列表
-            self.splitVal = 0 #进行分隔的特征的值(左子树小于)
-            self.splitJudge = math.inf#MSE
-            self.depth = depth
-
-            self.ans = 0
-            
-
-    def __init__(self,max_depth):
+    def __init__(self,max_depth=5):
         self._max_depth = max_depth
+        self.tree = DecisionTreeRegressor(max_depth = self._max_depth)
 
     def _get_data(self,data):
         '''
@@ -125,161 +105,19 @@ class DecisionTreeRegressor:
             提供数据和标签，进行训练
         '''
         #存储数据和标签
-        self.data = data
-        self.label = label
+
         #建立树
-        self._min_leaf_sz = int(len(data)/200) #简单回归树，最多允许2层。因此需要树尽量的平衡，不要出现太小的数据集
-        self.root = DecisionTreeRegressor.DecisionTreeNode(self._get_data(data),False,None,self._get_features(data.columns),0)
-        self._build_tree(self.root)
-
-    #根据一个节点建立一棵树
-    def _build_tree(self,treeNode):
-        '''
-            根据给定的节点进行树的建立
-        '''
-        #找到最佳的切分特征和最佳的切分点
-        isSplit = self._find_bestSplit(treeNode)
-        #判断是否结束建立树的过程（MSE差值、深度信息）
-        if (treeNode.depth + 1 < self._max_depth or self._max_depth == -1) and isSplit:
-            #如果没有结束，则分成左右子树，并继续递归
-            treeNode.isLeaf = False
-            lChildRow = [x for x in treeNode.dataRow if self.data[treeNode.splitCol].iloc[x] < treeNode.splitVal] #满足左子树要求的，行索引对应的数据全部小于切分值
-            rChildRow = [x for x in treeNode.dataRow if self.data[treeNode.splitCol].iloc[x] >= treeNode.splitVal] #满足右子树要求的，行索引对应的数据全部大于等于切分值
-            treeNode.lChild = DecisionTreeRegressor.DecisionTreeNode(lChildRow,False,treeNode,treeNode.featureCol,treeNode.depth+1)
-            treeNode.rChild = DecisionTreeRegressor.DecisionTreeNode(rChildRow,False,treeNode,treeNode.featureCol,treeNode.depth+1)
-            self._build_tree(treeNode.lChild)
-            self._build_tree(treeNode.rChild)
-        else:
-            #清洗切分痕迹，节点变为叶节点
-            treeNode.isLeaf = True
-            #计算平均值
-            labelSum = self.label[0].iloc[treeNode.dataRow].sum()
-            num = len(treeNode.dataRow)
-            treeNode.ans = labelSum/num
-
-        #如果结束了，就结束吧
-
-    def _compute_mse(self,square_sum,y_sum,number):
-        '''
-            自己计算MSE，展开后需要平方和，和以及数量
-        '''
-        return square_sum/number - (y_sum/number)**2
-
-    def _find_bestSplit(self,treeNode):
-        '''
-            对于给定的决策树节点，找到最佳的切分
-            树是回归树，因此衡量切分使用的是MSE
-            即切分要使得两边的MSE的加权和最小。
-            由于MSE计算上比较复杂，可能速度会比较慢，一个考虑是使用sklearn的MSE进行加速。如果是自己做的话，展开原式子可以发现MSE = 每个点的平方的和 - 每个点的和的平方除以N
-                因此计算MSE可以通过维护平方和、和以及数量三个数据来快速计算
-
-            答案写在treeNode.splitCol和treeNode.splitVal中,MSE保存在splitJudge中
-
-            返回一个bool值，这个值判断切分前后MSE的变化决定是否进行切分。
-                如果MSE在最佳切分后反而变大了，那么就取消切分操作。
-        '''
-        splitDiff = 0
-        isSplit = True
-        #遍历所有的特征
-        for col in treeNode.featureCol:
-            #拿到所有的行数据。对于DataFrame，使用DataFrame.values[feature][row]进行操作
-            aimData = self.data.values[treeNode.dataRow,col]
-            #进行排序和统计，取得遍历数据
-            sorted_idx = np.argsort(aimData)
-            sorted_data = aimData[sorted_idx]
-            sorted_label = self.label.values[sorted_idx,0]
-            #准备遍历时需要计算MSE而维护的量
-            lchild_number = 0
-            lchild_sum = 0
-            lchild_square_sum = 0
-            rchild_number = len(sorted_label)
-            rchild_sum = sorted_label.sum()
-            rchild_square_sum = (sorted_label**2).sum()
-            #节点的数据，作为后面计算MSE差值时要用到的量
-            node_number = rchild_number
-            node_sum = rchild_sum
-            node_square_sum = rchild_square_sum
-            #对所有的值进行遍历，保留自身最小的节点数量
-            for i in range(0,node_number-self._min_leaf_sz):
-                #遍历的时候计算MSE差值
-                xi , yi = sorted_data[i],sorted_label[i]
-
-                #修正数据
-                rchild_number -= 1
-                rchild_sum -= yi
-                rchild_square_sum -= yi**2
-                lchild_number += 1
-                lchild_sum += yi
-                lchild_square_sum += yi**2
-                if i < self._min_leaf_sz or xi == sorted_data[i+1]:#如果小于最小切分的叶节点数据数量，或是数据相同，则不进行切分
-                    continue
-
-                lchild_mse = self._compute_mse(lchild_square_sum,lchild_sum,lchild_number)
-                rchild_mse = self._compute_mse(rchild_square_sum,rchild_sum,rchild_number)
-                split_mse = (lchild_number*lchild_mse + rchild_number * rchild_mse)/node_number #MSE的加权和
-                
-                if split_mse < treeNode.splitJudge:
-                    treeNode.splitCol = col
-                    treeNode.splitVal = xi
-                    treeNode.splitJudge = split_mse
-                    splitDiff = node_number * (self._compute_mse(node_square_sum,node_sum,node_number) - split_mse)
-
-        if splitDiff < 0:
-            isSplit = False
-
-        return isSplit
-        
+        self.dataRow = self._get_data(data)
+        self.labelCol = self._get_features(range(data.shape[1]))
+        self.data = data[:,self.labelCol]
+        self.data = self.data[self.dataRow,:]
+        self.label = label
+        self.tree.fit(self.data,self.label.ravel())
+        print('fit over')
+        return self
 
     #接受数据，进行预测
     def predict(self,data):
-        '''
-        接受了数据，进行预测(思考了一下要不要做随机抽样，似乎不用吧)
-        返回一个一维数据向量
-        '''
-        ans = np.zeros(len(data))
-        #对每一行的数据
-        for i in range(len(data)):
-            row = data.iloc[i]
-            ans[i] = self._get_predict(row)
+        return self.tree.predict(data[:,self.labelCol])
 
-        return ans
 
-    #接受一个行的数据，在决策树上返回预测值
-    def _get_predict(self,row):
-        node = self.root
-        while not node.isLeaf:
-            if row[node.splitCol]<node.splitVal:
-                node = node.lChild
-            else:
-                node = node.rChild
-
-        return node.ans
-
-'''
-import pandas as pd                                                         
-import numpy as np
-#用于分析数据                 
-from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import StandardScaler
-
-dataSet = pd.read_csv('train1.csv',header=None)
-ansSet = pd.read_csv('label1.csv',header=None)
-
-X_train, X_test, y_train, y_test = train_test_split(dataSet, ansSet, test_size = 0.2, random_state = 0)
-
-myTree = DecisionTreeRegressor(5)
-data_num = 1000
-myTree.fit(X_train[:data_num],y_train[:data_num])
-y_ans = myTree.predict(X_train[data_num:2*data_num])
-y_correct = y_train[data_num:2*data_num].values[:,0]
-
-total = len(y_ans)
-correct = 0
-for i in range(len(y_ans)):
-    if y_ans[i] == y_correct[i]:
-        correct = correct+1
-
-print(correct/total)
-'''
